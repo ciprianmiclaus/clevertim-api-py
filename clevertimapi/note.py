92959ef9f3f83ef18f4ff6ddbaa4c7623c7c843d
Ciprian Miclaus <ciprianm@gmail.com>
2017-05-14 23:05:26 +0100

    support for note types and sms delivery status

--- clevertimapi/note.py
+++ clevertimapi/note.py
@@ -1,14 +1,42 @@
 from .compat import string_types
 from .session import Session
-from .endpoint import Endpoint, make_single_elem_property, make_single_elem_ref_property, make_multi_elem_ref_property
+from .endpoint import Endpoint, make_single_elem_property, make_single_elem_ref_property, make_multi_elem_ref_property, ValidationError
 
 
 class Note(Endpoint):
 
     ENDPOINT = '/note'
 
-    description = make_single_elem_property('desc', string_types, '', 'The text of the note')
+    class NOTE_TYPES(object):
+        PLAIN_NOTE = 'N'
+        DROPBOX_EMAIL = 'E'
+        EMAIL_NOTE = 'S'
+        SMS_NOTE = 'M'
 
+        ALL_VALID_VALUES = frozenset((PLAIN_NOTE, DROPBOX_EMAIL, EMAIL_NOTE, SMS_NOTE))
+        ALL_VALID_SET_VALUES = frozenset((PLAIN_NOTE, EMAIL_NOTE, SMS_NOTE))
+
+        @classmethod
+        def is_valid_note_type(cls, value):
+            if value not in cls.ALL_VALID_VALUES:
+                raise ValidationError("Invalid note type '%s'. Expected one of: %s" % (value, ', '.join(cls.ALL_VALID_VALUES)))
+            return True
+
+        @classmethod
+        def is_valid_set_note_type(cls, value):
+            if value not in cls.ALL_VALID_SET_VALUES:
+                raise ValidationError("Invalid note type '%s'. Expected one of: %s" % (value, ', '.join(cls.ALL_VALID_SET_VALUES)))
+            return True
+
+    class NOTE_DELIVERY_TYPES(object):
+        NO_EXTERNAL_DELIVERY = 'N'
+        SMS_DELIVERY_FAILED = 'F'
+        SMS_DELIVERY_SUCCEEDED = 'S'
+
+    description = make_single_elem_property('desc', string_types, '', 'The text of the note')
+    note_type = make_single_elem_property('type', string_types, NOTE_TYPES.PLAIN_NOTE, 'The type of note', validate_func=NOTE_TYPES.is_valid_set_note_type)
+
+    who = make_single_elem_ref_property('cust', 'ContactOrCompany', 'The contact or company this task is for')
     case = make_multi_elem_ref_property('case', 'Case', 'The case this note is filed under, if any.')
     opportunity = make_multi_elem_ref_property('opportunity', 'Opportunity', 'The opportunity this note is filed under, if any.')
 
@@ -19,7 +47,11 @@ class Note(Endpoint):
 
     comments = make_multi_elem_ref_property('comments', 'Comment', 'List of comments for this note')
 
-    # TODO: type, fmt, cust, delvry, headline, emailInfo
+    @property
+    def sms_delivery_info(self):
+        return self._content('delvry', Note.NOTE_DELIVERY_TYPES.NO_EXTERNAL_DELIVERY)
+
+    # TODO: fmt, headline, emailInfo
 
 
 Session.register_endpoint(Note)
