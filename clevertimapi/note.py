81a593ec0e555d61a6465c9aa227d8aa9585539e
Ciprian Miclaus <ciprianm@gmail.com>
2017-05-15 22:21:15 +0100

    add email info to notes

--- clevertimapi/note.py
+++ clevertimapi/note.py
@@ -1,6 +1,54 @@
 from .compat import string_types
 from .session import Session
-from .endpoint import Endpoint, make_single_elem_property, make_single_elem_ref_property, make_multi_elem_ref_property, ValidationError
+from .endpoint import Endpoint, make_single_elem_property, make_single_elem_ref_property, make_multi_elem_ref_property, ValidationError, ValueSerializer
+
+
+class EmailRecipient(ValueSerializer):
+
+    SENT = 's'
+    BOUNCED = 'b'
+
+    def __init__(self, content=None, session=None):
+        self._content = content or {}
+
+    @property
+    def email_address(self):
+        return self._content.get('e')
+
+    @property
+    def status(self):
+        return self._content.get('s')
+
+    @property
+    def bounce_reason(self):
+        return self._content.get('t')
+
+
+class EmailInfo(ValueSerializer):
+
+    def __init__(self, content=None, session=None):
+        self._content = content or {}
+        self.session = session
+
+    @property
+    def sender(self):
+        return self._content.get('f')
+
+    @property
+    def subject(self):
+        return self._content.get('s')
+
+    @property
+    def to(self):
+        return tuple([EmailRecipient(content=content, session=self.session) for content in self._content.get('t', ())])
+
+    @property
+    def cc(self):
+        return tuple([EmailRecipient(content=content, session=self.session) for content in self._content.get('c', ())])
+
+    @property
+    def bcc(self):
+        return tuple([EmailRecipient(content=content, session=self.session) for content in self._content.get('b', ())])
 
 
 class Note(Endpoint):
@@ -36,9 +84,9 @@ class Note(Endpoint):
     description = make_single_elem_property('desc', string_types, '', 'The text of the note')
     note_type = make_single_elem_property('type', string_types, NOTE_TYPES.PLAIN_NOTE, 'The type of note', validate_func=NOTE_TYPES.is_valid_set_note_type)
 
-    who = make_single_elem_ref_property('cust', 'ContactOrCompany', 'The contact or company this task is for')
-    case = make_multi_elem_ref_property('case', 'Case', 'The case this note is filed under, if any.')
-    opportunity = make_multi_elem_ref_property('opportunity', 'Opportunity', 'The opportunity this note is filed under, if any.')
+    who = make_multi_elem_ref_property('cust', 'ContactOrCompany', 'The contact or company this task is for')
+    case = make_single_elem_ref_property('case', 'Case', 'The case this note is filed under, if any.')
+    opportunity = make_single_elem_ref_property('opportunity', 'Opportunity', 'The opportunity this note is filed under, if any.')
 
     files = make_multi_elem_ref_property('files', 'File', 'List of files for this note')
     linked_files = make_multi_elem_ref_property('lfiles', 'LinkedFile', 'List of linked files for this note')
@@ -49,9 +97,13 @@ class Note(Endpoint):
 
     @property
     def sms_delivery_info(self):
-        return self._content('delvry', Note.NOTE_DELIVERY_TYPES.NO_EXTERNAL_DELIVERY)
+        return self._content.get('delvry', Note.NOTE_DELIVERY_TYPES.NO_EXTERNAL_DELIVERY)
 
-    # TODO: fmt, headline, emailInfo
+    @property
+    def email_info(self):
+        return EmailInfo(content=self._content.get('email'), session=self.session)
+
+    # TODO: fmt, headline
 
 
 Session.register_endpoint(Note)
